f087dcf164d3bc0fa7f43b01623f76e042697621
enamul-java <enamul.java@gmail.com>
2021-03-02 18:24:15 +0600

    Initial commit

---
+++ app/src/main/java/com/example/exceltopdf/ExcelToPdfUtil.java
@@ -0,0 +1,22 @@
+package com.example.exceltopdf;
+
+import android.util.Log;
+
+import com.aspose.cells.SaveFormat;
+import com.aspose.cells.Workbook;
+
+public class ExcelToPdfUtil {
+
+    public void convertToPdf(String excelFile, String pdfOutputPath)  {
+
+        try {
+            // Create Workbook to load Excel file
+            Workbook workbook = new Workbook(excelFile);
+            // Save the document in PDF format
+            workbook.save(pdfOutputPath, SaveFormat.PDF);
+        }catch (Exception e){
+            Log.e("ConverPdfError", e.toString());
+        }
+
+    }
+}
